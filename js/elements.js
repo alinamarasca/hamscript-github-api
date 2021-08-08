752e0d67d78aa38fc0e18ba07d0c1e6419bfda45
HYF <alinamarasca@gmail.com>
2021-08-08 13:51:07 +0200

    user doesnt exist

--- js/elements.js
+++ js/elements.js
@@ -1,26 +1,24 @@
-import { showData } from './handlers.js';
+import { showData } from "./handlers.js";
 
-export const search = document.createElement('div');
-search.className = 'search';
+export const search = document.createElement("div");
+search.className = "search";
 
-export const inputField = document.createElement('input');
-inputField.setAttribute('type', 'text');
-inputField.className = 'input';
-inputField.placeholder = 'find GitHub user'
+export const inputField = document.createElement("input");
+inputField.setAttribute("type", "text");
+inputField.className = "input";
+inputField.placeholder = "find GitHub user";
 
-
-const button =  document.createElement('button');
-button.innerText = 'search';
-button.addEventListener('click', showData);
+const button = document.createElement("button");
+button.innerText = "search";
+button.addEventListener("click", showData);
 
 search.append(inputField, button);
 
-
-export const result = document.createElement('div');
+export const result = document.createElement("div");
 result.className = "result";
 
-export const usersName = document.createElement('div');
-export const accCreated = document.createElement('div');
-export const pubRepos = document.createElement('div');
-export const url = document.createElement('div');
-export const location = document.createElement('div');
+export const usersName = document.createElement("div");
+export const accCreated = document.createElement("div");
+export const pubRepos = document.createElement("div");
+export const url = document.createElement("div");
+export const location = document.createElement("div");
--- js/handlers.js
+++ js/handlers.js
@@ -1,20 +1,34 @@
-import { usersName, accCreated, inputField, pubRepos, url, location, result } from './elements.js'
-import { client_id, client_secret } from './app.js';
+import {
+  usersName,
+  accCreated,
+  inputField,
+  pubRepos,
+  url,
+  location,
+  result,
+} from "./elements.js";
+import { client_id, client_secret } from "./app.js";
 
+export const fetchUsers = async (user) => {
+  const URL = `https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`;
 
-export const fetchUsers = async(user) => {
-const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
-const data = await api_call.json();
-return { data };
+  const encodedURL = encodeURI(URL);
+  const response = await fetch(encodedURL);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}\n-> ${URL}`);
+  }
+  const data = await response.json();
+
+  return { data };
 };
 
-
-export const showData = () => {
-
+export const showData = async () => {
+  try {
+    result.innerHTML = "";
     result.append(usersName, accCreated, pubRepos, url, location);
 
-    fetchUsers(inputField.value).then((response) => {
-    
+    const response = await fetchUsers(inputField.value);
+
     usersName.innerHTML = `<span class='field'>name:</span> ${response.data.name}`;
 
     let date = new Date(response.data.created_at);
@@ -23,10 +37,11 @@ export const showData = () => {
     pubRepos.innerHTML = `<span class='field'>public repos:</span>  ${response.data.public_repos}`;
 
     url.innerHTML = `<span class='field'>github url:</span> <a href = ${response.data.html_url}> ${response.data.html_url}</a>`;
-    
 
     location.innerHTML = `<span class='field'>current location:</span> ${response.data.location}`;
-    })
-
-    inputField.value = '';
-}
+  } catch (error) {
+    console.log(error);
+    result.innerHTML = `User '${inputField.value}' doesn't exist`;
+  }
+  inputField.value = "";
+};
